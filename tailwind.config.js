6a074f95e7381800934f50dc5f8462645d34d65b
devseed007 <devseed007@gmail.com>
2025-05-08 18:21:52 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  darkMode: 'class', // Enable class-based dark mode
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
